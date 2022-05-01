cc850943cab3e68fed262d9d8dc91cbd67fdf748
Arogya Dhakal <arogya@email.unc.edu>
2022-04-30 21:58:23 -0400

    hopefull this works

--- src/App.js
+++ src/App.js
@@ -44,12 +44,13 @@ function App() {
     let currentA03 = document.querySelector('#a03').value
     let currentA04 = document.querySelector('#a04').value
     let currentA05 = document.querySelector('#a05').value
+    let currentMidterm = document.querySelector('#midterm').value
     let currentE01 = document.querySelector('#e01').value
     let currentE02 = document.querySelector('#e02').value
     if(currentSum==='')
     return;
 // 2. This line is where you add the points all together. Add your other variable references here. Make sure that they match what is above and what is below in the HTML form
-    let sum = parseInt(currentA00) + parseInt(currentA01) + parseInt(currentA02) + parseInt(currentA03) + parseInt(currentA04) + parseInt(currentA05) + parseInt(currentE01) + parseInt(currentE02);
+    let sum = parseInt(currentA00) + parseInt(currentA01) + parseInt(currentA02) + parseInt(currentA03) + parseInt(currentA04) + parseInt(currentA05) + parseInt(currentE01) + parseInt(currentE02) + parseFloat(currentMidterm);
     setCurrentSum(sum);
   }
 
@@ -68,9 +69,11 @@ function App() {
             <input type="text" id="a01" placeholder="a01" /><br/><br/>
             <input type="text" id="a02" placeholder="a02" /><br/><br/>
             <input type="text" id="a03" placeholder="a03" /><br/><br/>
-            <input type="text" id="a01" placeholder="a01" /><br/><br/>
-            <input type="text" id="a01" placeholder="a01" /><br/><br/>
-            <input type="text" id="a01" placeholder="a01" /><br/><br/>
+            <input type="text" id="a04" placeholder="a04" /><br/><br/>
+            <input type="text" id="a05" placeholder="a05" /><br/><br/>
+            <input type="text" id="midterm" placeholder="midterm" /><br/><br/>
+            <input type="text" id="e01" placeholder="eo1" /><br/><br/>
+            <input type="text" id="e02" placeholder="e02" /><br/><br/>
             <button onClick={Add}>Add</button>
             <button onClick={Clear}>Clear</button>
             <label for="result"><b>Result:</b></label>
